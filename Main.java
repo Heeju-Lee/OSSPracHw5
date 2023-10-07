3560e77fac6e04ba30a57f2ad472e7b4368b7e84
Heeju-Lee <lhj2778@dongguk.edu>
2023-10-07 13:40:57 +0900

    ossp2 branch

--- Main.java
+++ Main.java
@@ -4,22 +4,26 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        
+
         System.out.print("이름을 입력하세요: ");
         String name = sc.nextLine();
-        
+
         System.out.print("학번을 입력하세요: ");
         String id = sc.nextLine();
-        
+
         System.out.print("학과를 입력하세요: ");
         String dep = sc.nextLine();
-        
+
+        System.out.print("학년을 입력하세요: ");
+        int grade = sc.nextInt();
+
         //한 줄 엔터
-        System.out.println();        
+        System.out.println();
         System.out.println("<출력>");
         System.out.println("이름: " + name);
-        System.out.println("학번: " + id);        
-        System.out.println("학과: " +dep);
+        System.out.println("학번: " + id);
+        System.out.println("학과: " + dep);
+        System.out.println("학년: " + grade);
 
         sc.close();
     }
